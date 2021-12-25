bf29c8a87dbae74509e2f63e94bc9f4a44776f9b
jobayed003 <jobayed803@gmail.com>
2021-12-25 21:04:18 +0600

    added modals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
+import Cart from './Components/Cart/Cart';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
 
 const App = () => {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
